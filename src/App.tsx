0d4b5d80266b1c334dcfaf3b81d01b261b92479c
Jefferson Silva Brito Cabral <jscabral@firjan.com.br>
2022-07-07 10:04:56 -0300

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 4rem;
-  color: #c3c3c3;
-`
-
 export function App() {
   return (
     <div className="App">
-      <Title>Hello world</Title>
+      <h1>Hello world</h1>
     </div>
   );
 }
